ec4564083e5c3d6fbb24bac7ba215277d43c10e8
HasanainChishti <hasanainchisti2003@gmail.com>
2025-10-16 02:04:56 +0530

    update

---
+++ api/cors.js
@@ -0,0 +1,21 @@
+export default async function handler(req, res) {
+  const targetUrl = req.query.url;
+  if (!targetUrl) {
+    return res.status(400).json({ error: "Missing ?url= parameter" });
+  }
+
+  try {
+    const response = await fetch(targetUrl, {
+      headers: {
+        "User-Agent": "Mozilla/5.0",
+        "Accept": "application/json",
+      },
+    });
+
+    const data = await response.text();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
